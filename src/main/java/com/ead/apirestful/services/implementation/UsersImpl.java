faf5c190b8202ea30c0939e8732633ee9da9f1a6
Estefania Aguirre <estefaniaaguirre.plv@gmail.com>
2021-08-18 16:33:02 -0500

    Se agrega todos los métodos abstractos

--- src/main/java/com/ead/apirestful/services/implementation/UsersImpl.java
+++ src/main/java/com/ead/apirestful/services/implementation/UsersImpl.java
@@ -1,5 +1,49 @@
 package com.ead.apirestful.services.implementation;
 
-public class UsersImpl {
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+
+import com.ead.apirestful.dto.UsersDTO;
+import com.ead.apirestful.services.interfaces.IUsersService;
+
+public class UsersImpl implements IUsersService {
+
+	@Override
+	public Page<UsersDTO> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Optional<UsersDTO> findByUsername(String username) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Optional<UsersDTO> findByUserId(int userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void save(UsersDTO user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void saveAll(List<UsersDTO> users) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(int userId) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
