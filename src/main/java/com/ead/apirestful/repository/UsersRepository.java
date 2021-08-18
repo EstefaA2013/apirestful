8b2b2338c0cca93ecac8a827b5116b585dc40aab
Estefania Aguirre <estefaniaaguirre.plv@gmail.com>
2021-08-18 15:10:16 -0500

    En el repositorio buscar por username
    En este paso se busca por el nombre de usuario, se utilizo
    transational para que la consulta sea solo para lectura

--- src/main/java/com/ead/apirestful/repository/UsersRepository.java
+++ src/main/java/com/ead/apirestful/repository/UsersRepository.java
@@ -1,11 +1,16 @@
 package com.ead.apirestful.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.ead.apirestful.entities.Users;
 
 @Repository
 public interface UsersRepository extends CrudRepository<Users, Integer> {
 
+	@Transactional(readOnly = true)
+	Optional<Users> findByUsername(String username);
 }
