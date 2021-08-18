f901a09adbc38deadd3a6b576ec83119808963b4
Estefania Aguirre <estefaniaaguirre.plv@gmail.com>
2021-08-18 13:48:08 -0500

    Se crea la entidad usuario
    Se crean los atributos que estan en la tabla de la base de datos
    utilizando el Lombok

---
+++ src/main/java/com/ead/apirestful/entities/Users.java
@@ -0,0 +1,51 @@
+package com.ead.apirestful.entities;
+
+import java.io.Serializable;
+import java.sql.Date;
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class Users implements Serializable {
+	
+	@Id
+	@Column(name = "user_id")
+	private int id;
+	@Column(name = "firstname")
+	private String firstname;
+	@Column(name = "lastname")
+	private String lastname;
+	@Column(name = "username")
+	private String username;
+	@Column(name = "password")
+	private String password;
+	@Column(name = "created_at")
+	@Temporal(TemporalType.TIMESTAMP)
+	private LocalDateTime createdAt;
+	@Column(name = "updated_at")
+	@Temporal(TemporalType.TIMESTAMP)
+	private LocalDateTime updatedAt;
+	
+	@PrePersist
+	public void prePersist() {
+		this.createdAt = LocalDateTime.now();
+	}
+	
+	@PreUpdate
+	public void preUpdate() {
+		this.updatedAt = LocalDateTime.now();
+	}
+
+}
