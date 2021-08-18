3ee0b4341419ed082cd844956969cad024960719
Estefania Aguirre <estefaniaaguirre.plv@gmail.com>
2021-08-18 16:47:12 -0500

    Se agrega la anotación Component y Autowired

--- src/main/java/com/ead/apirestful/services/implementation/UsersImpl.java
+++ src/main/java/com/ead/apirestful/services/implementation/UsersImpl.java
@@ -3,13 +3,19 @@ package com.ead.apirestful.services.implementation;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-
+import org.springframework.stereotype.Component;
 import com.ead.apirestful.dto.UsersDTO;
+import com.ead.apirestful.repository.UsersRepository;
 import com.ead.apirestful.services.interfaces.IUsersService;
 
+@Component
 public class UsersImpl implements IUsersService {
 
+	@Autowired
+	private UsersRepository usersRepository;
+	
 	@Override
 	public Page<UsersDTO> findAll() {
 		// TODO Auto-generated method stub
