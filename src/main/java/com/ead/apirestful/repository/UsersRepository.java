5b33841ba12f0769e130d183c59cf493beee6e7a
Estefania Aguirre <estefaniaaguirre.plv@gmail.com>
2021-08-18 16:14:33 -0500

    En el repositorio se hace un Override
    Este nos devuelve un Page de los usuarios, para hacer una conversion de
    estos al DTO

--- src/main/java/com/ead/apirestful/repository/UsersRepository.java
+++ src/main/java/com/ead/apirestful/repository/UsersRepository.java
@@ -2,6 +2,7 @@ package com.ead.apirestful.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,4 +14,7 @@ public interface UsersRepository extends CrudRepository<Users, Integer> {
 
 	@Transactional(readOnly = true)
 	Optional<Users> findByUsername(String username);
+	
+	@Override
+	Page<Users> findAll();
 }
