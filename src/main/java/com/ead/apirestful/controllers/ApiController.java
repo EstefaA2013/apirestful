b13267e54ccbcff7ce83e38dc32174040a629559
Estefania Aguirre <estefaniaaguirre.plv@gmail.com>
2021-08-19 14:34:53 -0500

    Se creo el método buscar por nombre

--- src/main/java/com/ead/apirestful/controllers/ApiController.java
+++ src/main/java/com/ead/apirestful/controllers/ApiController.java
@@ -5,6 +5,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +23,11 @@ public class ApiController {
 	public ResponseEntity<Object> index() {
 		
 		return ResponseEntity.ok(this.usersService.findAll());
+	}
+	
+	@GetMapping(value = "/by/{username}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Object> findByUsername(@PathVariable("username") String username) {
+		
+		return ResponseEntity.ok(this.usersService.findByUsername(username));
 	}
 }
