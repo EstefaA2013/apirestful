41cde87a533321b6291a99588941fc5241a762d8
Estefania Aguirre <estefaniaaguirre.plv@gmail.com>
2021-08-19 16:51:14 -0500

    Se agregó el método save

--- src/main/java/com/ead/apirestful/controllers/ApiController.java
+++ src/main/java/com/ead/apirestful/controllers/ApiController.java
@@ -6,9 +6,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ead.apirestful.dto.UserRequest;
 import com.ead.apirestful.services.interfaces.IUsersService;
 
 @CrossOrigin("*")
@@ -29,5 +32,14 @@ public class ApiController {
 	public ResponseEntity<Object> findByUsername(@PathVariable("username") String username) {
 		
 		return ResponseEntity.ok(this.usersService.findByUsername(username));
+	}
+	
+	@PostMapping(value = "/save", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Object> saveUser(@RequestBody UserRequest request)
+	{
+	
+		this.usersService.save(request);
+		return ResponseEntity.ok(Boolean.TRUE);
+		
 	}
 }
