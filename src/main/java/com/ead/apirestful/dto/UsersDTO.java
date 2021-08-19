e045d31e0fdf9bd4e3ac7fe95d23d1727f4d3b67
Estefania Aguirre <estefaniaaguirre.plv@gmail.com>
2021-08-19 15:41:53 -0500

    Se eliminaron los gets y sets
    Esta modificación se hizo porque no se necesitan los gets y sets
    porque ya se tiene la anotación Data y se hizo una anotacion Json

--- src/main/java/com/ead/apirestful/dto/UsersDTO.java
+++ src/main/java/com/ead/apirestful/dto/UsersDTO.java
@@ -2,6 +2,9 @@ package com.ead.apirestful.dto;
 
 import java.io.Serializable;
 import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Data;
 
 @Data
@@ -11,48 +14,14 @@ public class UsersDTO implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private int user_id;
+	private int id;
 	private String firstname;
 	private String lastname;
 	private String username;
-	private Date created_at;
-	private Date updated_at;
-	public int getId() {
-		return user_id;
-	}
-	public void setId(int user_id) {
-		this.user_id = user_id;
-	}
-	public String getFirstname() {
-		return firstname;
-	}
-	public void setFirstname(String firstname) {
-		this.firstname = firstname;
-	}
-	public String getLastname() {
-		return lastname;
-	}
-	public void setLastname(String lastname) {
-		this.lastname = lastname;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public Date getCreated_at() {
-		return created_at;
-	}
-	public void setCreated_at(Date created_at) {
-		this.created_at = created_at;
-	}
-	public Date getUpdated_at() {
-		return updated_at;
-	}
-	public void setUpdated_at(Date updated_at) {
-		this.updated_at = updated_at;
-	}
+	@JsonProperty(value = "created_at")
+	private Date createdAt;
+	@JsonProperty(value = "updated_at")
+	private Date updatedAt;
 	
 	
 }
