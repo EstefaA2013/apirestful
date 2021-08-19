0f6099e46d31692c2e471b4ced5aeb7d6ffedd37
Estefania Aguirre <estefaniaaguirre.plv@gmail.com>
2021-08-19 11:37:38 -0500

    Se modifico la clase ApiController
    Cuando se consulte por el GetMapping este reciba una lista de objectos

--- src/main/java/com/ead/apirestful/controllers/ApiController.java
+++ src/main/java/com/ead/apirestful/controllers/ApiController.java
@@ -1,19 +1,26 @@
 package com.ead.apirestful.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ead.apirestful.services.interfaces.IUsersService;
+
 @CrossOrigin("*")
 @RestController
-@RequestMapping("/")
+@RequestMapping("/users")
 public class ApiController {
 	
-	@GetMapping("Example")
+	@Autowired
+	private IUsersService usersService;
+	
+	@GetMapping(value = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Object> index() {
 		
-		return ResponseEntity.ok("OK");
+		return ResponseEntity.ok(this.usersService.findAll());
 	}
 }
