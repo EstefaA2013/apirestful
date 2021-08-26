b50479e69361f5369d49c4ad3bfa25f1acb6beb7
Estefania Aguirre <estefaniaaguirre.plv@gmail.com>
2021-08-26 15:22:00 -0500

    Se modifica la clase AuthService
    Se modifico la linea para generar el token, mostrando información del
    usuario

--- src/main/java/com/ead/apirestful/services/interfaces/AuthService.java
+++ src/main/java/com/ead/apirestful/services/interfaces/AuthService.java
@@ -2,26 +2,48 @@ package com.ead.apirestful.services.interfaces;
 
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.ead.apirestful.dto.JwtResponse;
-
+import com.ead.apirestful.dto.JwtUserDTO;
+import com.ead.apirestful.security.JwtIO;
+import com.ead.apirestful.utils.helpers.DateUtils;
 
 @Service
 public class AuthService {
 
-public JwtResponse login(String clientId, String clientSecret) {
+	@Autowired
+	private JwtIO jwtIO;
+	@Autowired
+	private DateUtils dateUtils;
+	
+	@Value("${ead.jwt.token.expires-in}")
+	private int EXPIRES_IN;
+	
+	public JwtResponse login(String clientId, String clientSecret) {
+
+		UUID uid = UUID.randomUUID();
+
+		JwtUserDTO user = JwtUserDTO.builder()
+				.name("Estefania")
+				.lastname("Aguirre")
+				.role("ADMIN")
+				.country("Colombia")
+				.uid(uid.toString())
+				.build();
 		
 		
 		JwtResponse jwt = JwtResponse.builder()
 				.tokenType("bearer")
-				.accessToken("kajushdiffhf")
-				.issuedAt(System.currentTimeMillis()+"")
+				.accessToken(jwtIO.generateToken(user))
+				.issuedAt(dateUtils.getDateMillis() +"")
 				.cliendId(clientId)
-				.expiresIn(3600)
+				.expiresIn(EXPIRES_IN)
 				.build();
-		
+
 		return jwt;
 	}
-	
+
 }
